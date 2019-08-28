19dc370b8a0de0c8b6a42dc5a61ca3f8cc7a045a
Rafaella Weiss <weissrafaella@MacBook-Pro-de-Rafaella.local>
2019-08-28 18:13:40 -0300

    questão 1 lista 1 inp

---
+++ lista_1.py
@@ -0,0 +1,10 @@
+# Programa para mostrar a diferença entre dois números
+
+a = float(input('Digite o valor do primeiro número: '))
+b = float(input('Digite o valor do segundo número: '))
+
+c = a - b
+
+
+print('O valor da subtração é: ', c)
+
