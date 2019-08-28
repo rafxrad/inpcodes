adc183f2ce034244f634d284157d09d8987e4318
Rafaella Weiss <weissrafaella@MacBook-Pro-de-Rafaella.local>
2019-08-28 19:36:56 -0300

    questão 5, lista 1.

--- lista_5.py
+++ lista_5.py
@@ -2,3 +2,11 @@
 
 segundos = int(input('Digite o valor dos segundos: '))
 
+segundos_rest = int(segundos % 60)
+minutos = int(segundos / 60)
+minutos_rest = int(minutos % 60)
+hora_rest = int(minutos / 60)
+
+hms = print(f'{hora_rest}:{minutos_rest}:{segundos_rest}')
+
+
