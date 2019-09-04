7e252a31f2e5a605648886325e716873ee86f621
Rafa <54604961+weissxrafa@users.noreply.github.com>
2019-09-04 15:09:48 -0300

    lista12.py
    questão 12,  lista 1.

---
+++ Questão12.py
@@ -0,0 +1,73 @@
+# Programa de pesquisa de dados
+
+# Coletar dados de idade, sexo e salário dos usuários.
+
+entradas = 0
+mulheres = 0
+homens = 0
+nãoInformados = 0
+recebeMais1500 = 0
+recebeMenos1500 = 0
+Maior = 0
+Menor = 1000000000000000000000
+idadeMaior = 0
+sexoMaior = 0
+idadeMenor = 0
+sexoMenor = 0
+acumulaValor = 0
+
+while True: 
+
+    idade = int(input('Digite a idade (digite uma idade negativa para sair do programa): '))
+
+    if idade < 0:
+        break
+
+    sexo = input(('Digite M para Masculino, F para Feminino e NF para não informado: '))
+    valor = float(input('Valor do salário: '))
+
+    entradas += 1
+    acumulaValor += valor
+    
+   
+    if valor > Maior:
+        Maior = valor
+        idadeMaior = idade
+        sexoMaior = sexo
+
+        if valor < Menor:
+            Menor = valor 
+            idadeMenor = idade
+            sexoMenor = sexo
+    else:
+        Menor = valor
+        idadeMenor = idade
+        sexoMenor = sexo
+        
+
+    if sexo == 'M':
+        homens += 1 
+
+        if valor < 1500:
+            recebeMenos1500 += 1
+            porcentagem = recebeMenos1500 / entradas
+
+    elif sexo == 'F':
+        mulheres += 1
+
+        if valor > 1500:
+            recebeMais1500 += 1
+
+    else: 
+        nãoInformados += 1
+
+
+
+    mediadeSalarios =  float(acumulaValor / entradas)
+
+print('A média dos salários é: R$ ', mediadeSalarios)
+print('A quantidade de mulheres que recebem mais que R$ 1500: ', recebeMais1500)
+print('A porcentagem de homens que recebem menos que R$ 1500: ', porcentagem)
+print('A idade e o sexo da pessoa que recebe o maior e o menor valor: ')
+print('Menor: Idade: ' +  str(idadeMenor) + ' Sexo: ' + str(sexoMenor))
+print('Maior: Idade: ' +  str(idadeMaior) + ' Sexo: ' +  str(sexoMaior))
